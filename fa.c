251968f36850a929ddfb7bd313f00a8461686ddf
Sean Beagle <30675442+SeanBeagle@users.noreply.github.com>
2020-03-25 10:48:56 -0600

    Update fa.c

--- fa.c
+++ fa.c
@@ -1,75 +1,106 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
+/******************************************************************************
+  FILE: fa.c
+ ABOUT: Returns JSON of FastA summary statistics.
+AUTHOR: Sean Beagle
+ EMAIL: SeanBeagle@gmail.com
+   URL: http://www.SeanBeagle.com
+        http://www.StrongLab.org
+******************************************************************************/
 
-struct Params {
-    bool no_headers;
-};
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdbool.h>
 
-struct Params params;
+// FORWARD DECLARATIONS: 
+void newRecord();
+void printRecordStats();
+void printFastaStats();
+void readFasta();
+void clearRecordResidues();
+bool isHeader(char *line);
 
-struct Fasta {
-    FILE *file;
-    char *filename;
-    unsigned long contigs, basepairs, A, T, C, G;
-};
-struct Fasta fa;
+// GLOBAL VARIABLES:
+FILE *file;
+char filename[255];
+unsigned int num_records = 0;
+unsigned long total_residues[256], total_size = 0; 
+unsigned long record_residues[256], record_size = 0;
 
-void read_fasta();
-float gc_content();
-
+// MAIN:
 int main(int argc, char *argv[]) {
-    if (argc == 1) { /* Check for filename argument */
-        printf("Usage: %s [FILE]\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    if (fa.file = fopen(argv[1], "r")) { /* This is a file */
-        fa.filename = argv[1];
-        read_fasta();
-    } else { /* This is not a file */
-        printf("ERROR: could not read %s\n", argv[1]);
-        return EXIT_FAILURE;
-    }
+  if (argc == 2 && (file = fopen(argv[1], "r"))) {
+    strcpy(filename, argv[1]);
+    readFasta();
+    fclose(file);
+  } else {
+    return EXIT_FAILURE;
+  }
 }
 
-void read_fasta() {
-    char *line = NULL;
-    size_t len = 0;
-    while((getline(&line, &len, fa.file)) != -1) {
-        if (line[0] == '>') {
-            ++fa.contigs;
-        } else {
-            fa.basepairs += strlen(line)-1;
-            int c, i = 0;
-            while((c = line[i++]) != '\0') {
-                switch(c) {
-                    case 'A': ++fa.A;
-                    break;
-                    case 'T': ++fa.T;
-                    break;
-                    case 'C': ++fa.C;
-                    break;
-                    case 'G': ++fa.G;
-                    break;
-                    default: break;
-                }
-            }
-        }
-    }
-    fclose(fa.file);
+void readFasta() {
+  clearRecordResidues();
 
-    printf("contigs=%d,", fa.contigs);
-    printf("basepairs=%d,", fa.basepairs);
-    printf("A=%d,T=%d,C=%d,G=%d,", fa.A, fa.T, fa.C, fa.G);
-    printf("GC=%.2f\n", gc_content());
+  printf("{\"records\": [");
+
+  char *line;
+  size_t len = 0;
+
+  while ((getline(&line, &len, file)) != -1) {
+    if (isHeader(line)) {
+      if (num_records > 0) { 
+        printRecordStats();
+        printf(", ");
+      }
+      // remove last char if newline
+      char last_char = line[strlen(line)-1];
+      line[strlen(line)-1] = (last_char == '\n') ? '\0' : last_char;
+      // begin printing record
+      printf("{\"id\": \"%s\"", line+1);
+      newRecord();
+    } else {
+      total_size += strlen(line) - 1;
+      record_size += strlen(line) - 1;
+      int residue, i = 0;
+      while ((residue = toupper(line[i++])) != '\0' && residue != '\n') {
+        ++record_residues[residue];
+        ++total_residues[residue];
+      }
+    }
+  }
+  printRecordStats();
+  printf("], ");
+  printFastaStats();
 }
 
-float gc_content() {
-    float atcg = fa.A + fa.T + fa.C + fa.G;
-    float gc = fa.G + fa.C;
-    if (atcg == 0 || gc == 0)
-        return 0;
-    return gc / atcg;
+/* Clear previous records data */
+void newRecord() {
+  num_records++;
+  record_size = 0;
+  clearRecordResidues();
+}
+
+bool isHeader(char *line) {
+  return line[0] == '>';
+}
+
+void clearRecordResidues() {
+  for (int i = 0; i < 256; i++)
+    record_residues[i] = 0;
+}
+
+void printRecordStats() {
+  for (int i = 0; i < 256; i++) {
+    if (record_residues[i] > 0) printf(", \"%c\": %lu", i, record_residues[i]);
+  }
+    printf(", \"basepairs\": %d}", record_size);
+}
+
+void printFastaStats() {
+  printf("\"fasta\": {\"%s\"", filename);
+  for (int i = 0; i < 256; i++) {
+    if (total_residues[i] > 0) printf(", \"%c\": %lu", i, total_residues[i]);
+  }
+  printf(", \"basepairs\": %d}\n", total_size);
 }
